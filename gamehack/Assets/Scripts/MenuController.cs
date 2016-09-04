2360eff84486b0d6bb2372df456a638324a4726b
Michal Kruk <kruczy@gmail.com>
2016-09-04 19:15:45 +0100

    add menu

---
+++ gamehack/Assets/Scripts/MenuController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
