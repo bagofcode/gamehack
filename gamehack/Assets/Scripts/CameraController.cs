a81c2892c7271decb6aa494c08fd63e4ca7796ba
Michal Kruk <kruczy@gmail.com>
2016-09-04 18:31:55 +0100

    improve camera

---
+++ gamehack/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Rigidbody2D target;
+    public float followSpeed = 2.0f;
+    public Vector2 distanceInFront;
+
+    void FixedUpdate()
+    {
+        Vector2 desiredPosition = new Vector2(target.velocity.x * distanceInFront.x, target.velocity.y * distanceInFront.y) + target.position;
+
+        Vector2 targetPosition = Vector2.Lerp(this.transform.position, desiredPosition, Time.deltaTime * followSpeed);
+
+        this.transform.position = new Vector3(targetPosition.x, targetPosition.y, this.transform.position.z);
+    }
+}
