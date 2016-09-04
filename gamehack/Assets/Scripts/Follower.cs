685eaf545a527d2c67b4218d3044b7035217d67f
Michal Kruk <kruczy@gmail.com>
2016-09-04 11:21:16 +0100

    Simple player&terrain setup

---
+++ gamehack/Assets/Scripts/Follower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    public Transform target;
+    public Vector2 offset; //TODO: leaning etc
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 cameraPositionTarget = (Vector2)target.position + offset;
+
+        this.transform.position = new Vector3(cameraPositionTarget.x, cameraPositionTarget.y, this.transform.position.z);
+	}
+}
