1a1125064cf9040665c4a3e68d5b6476fdec6a77
Michal Kruk <kruczy@gmail.com>
2016-09-04 12:36:10 +0100

    Movement

--- gamehack/Assets/Scripts/PlayerController.cs
+++ gamehack/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 
     public Collider2D groundDetector;
     public float jumpForce = 1.0f;
+    public float walkSpeed = 10.0f;
 
     private int groundLayer;
     private Rigidbody2D rb;
@@ -24,6 +25,11 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        Walk(Input.GetAxis("Horizontal"));
+    }
+
     bool CanJump()
     {
         return groundDetector.IsTouchingLayers(groundLayer);
@@ -32,5 +38,10 @@ public class PlayerController : MonoBehaviour
     void Jump()
     {
         this.rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void Walk(float direction)
+    {
+        this.transform.position += Vector3.right * (direction * walkSpeed * Time.deltaTime);
     }
 }
