ad1b860e255fbaf58f938909ffef194651434262
Michal Kruk <kruczy@gmail.com>
2016-09-04 14:14:34 +0100

    Shooting a bullet

---
+++ gamehack/Assets/Scripts/ShootingController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingController : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform origin;
+
+    public void Fire(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        var bulletInstance = (GameObject)Instantiate(bullet, origin.position, rotation);
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = direction.normalized * 20;
+    }
+}
