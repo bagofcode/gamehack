49bb00652dbcce7e1bb0961ccced3966831da808
Michal Kruk <kruczy@gmail.com>
2016-09-25 13:29:18 +0100

    send info about targetting

--- gamehack/Assets/Scripts/AIShooterController.cs
+++ gamehack/Assets/Scripts/AIShooterController.cs
@@ -1,24 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum AIShooterState
+{
+    TARGETTING, WAITING
+}
+
+public struct AIShooterInfo
+{
+    public Vector2 from;
+    public Vector2? target;
+    public AIShooterState state;
+}
+
 public class AIShooterController : ShootingController
 {
     public Transform target;
     public float shootDelay = 1f;
+    public float range = 2;
 
     private float nextShot;
+    private float rangeSqr;
+
+    private AIShooterInfo info;
 
     void Awake()
     {
         nextShot = Time.realtimeSinceStartup;
+        rangeSqr = range * range;
     }
 
     void Update()
     {
+        this.info.from = this.gameObject.transform.position;
+
         if (nextShot < Time.realtimeSinceStartup)
         {
-            nextShot = Time.realtimeSinceStartup + shootDelay;
-            Fire(target.position - this.transform.position);
+            Vector2 direction = target.position - this.transform.position;
+            this.info.target = direction;
+
+            if (rangeSqr >= direction.sqrMagnitude)
+            {
+                nextShot = Time.realtimeSinceStartup + shootDelay;
+                Fire(direction);
+            }
         }
+        else {
+            this.info.target = null;
+        }
+
+        this.gameObject.SendMessage("OnAITargetting", this.info, SendMessageOptions.DontRequireReceiver);
     }
 }
--- gamehack/Assets/Scripts/EnemyController.cs
+++ gamehack/Assets/Scripts/EnemyController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
-
     void OnDeath()
     {
         Destroy(this.gameObject);
