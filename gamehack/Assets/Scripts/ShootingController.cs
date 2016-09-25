754ef1a37db500f47903328398ca12ab1155005b
Michal Kruk <kruczy@gmail.com>
2016-09-25 17:37:19 +0100

    turret firing better

--- gamehack/Assets/Scripts/ShootingController.cs
+++ gamehack/Assets/Scripts/ShootingController.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Assets.Scripts.Util;
 
 public class ShootingController : MonoBehaviour
 {
     public GameObject bullet;
     public Transform origin;
+    public GameObject ignore;
 
     public void Fire(Vector2 direction)
     {
         var rotation = QuaternionUtil.FromDirection2D(direction);
-
+        
         var bulletInstance = (GameObject) Instantiate(bullet, origin.position, rotation);
         bulletInstance.GetComponent<Rigidbody2D>().velocity = direction.normalized*20;
-        bulletInstance.GetComponent<BulletController>().ignore = this.gameObject;
+        bulletInstance.GetComponent<BulletController>().ignore = ignore;
     }
 }
